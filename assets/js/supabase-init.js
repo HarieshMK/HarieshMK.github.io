ec0325a43fe4010e5a9c17335aca5f6f117ccc7d
HarieshMK <154051561+HarieshMK@users.noreply.github.com>
2026-03-10 18:04:33 +0530

    Create supabase-init.js

---
+++ assets/js/supabase-init.js
@@ -0,0 +1,7 @@
+// Initialize Supabase
+const supabaseUrl = 'https://verivhhyvwvahmxibwkz.supabase.co';
+const supabaseKey = 'sb_publishable_CqlRq_Y5c1Em2JneO-Z13A_ZpC2RE9K';
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase initialized successfully!");
