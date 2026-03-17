d81e785007bcd417e4142d29e77910e23409c0e8
HarieshMK <154051561+HarieshMK@users.noreply.github.com>
2026-03-17 11:48:34 +0530

    Create investment-options.js

---
+++ assets/js/investment-options.js
@@ -0,0 +1,22 @@
+// assets/js/investment-options.js
+const InvestmentRegistry = {
+    "Bank savings account": { liquidity: "Very high", type: "Debt", returns: 2.5 },
+    "RD": { liquidity: "Very high", type: "Debt", returns: 6.0 },
+    "FD": { liquidity: "Very high", type: "Debt", returns: 6.5 },
+    "LIC plan": { liquidity: "Very low", type: "Debt", returns: 5.0 },
+    "PPF": { liquidity: "Very low", type: "Debt", returns: 7.1 },
+    "SSY": { liquidity: "Very low", type: "Debt", returns: 8.1 },
+    "NSC": { liquidity: "Very low", type: "Debt", returns: 7.7 },
+    "KVP": { liquidity: "Very low", type: "Debt", returns: 7.5 },
+    "EPF": { liquidity: "Very low", type: "Debt", returns: 8.15 },
+    "EPS": { liquidity: "Very low", type: "Debt", returns: 0.0 },
+    "Liquid fund": { liquidity: "Very high", type: "Debt", returns: 6.0 },
+    "Arbitrage fund": { liquidity: "High", type: "Debt", returns: 6.5 },
+    "Gold plan": { liquidity: "Low", type: "Gold", returns: 6.5 },
+    "Nifty 50": { liquidity: "High", type: "Equity", returns: 10.0 },
+    "Smallcap 250": { liquidity: "High", type: "Equity", returns: 13.5 },
+    "Flexi cap": { liquidity: "High", type: "Equity", returns: 12.0 },
+    "Shares": { liquidity: "High", type: "Equity", returns: 12.0 },
+    "NPS": { liquidity: "Very low", type: "Hybrid", returns: 9.0 },
+    "Crypto": { liquidity: "Very high", type: "Crypto", returns: 18.0 }
+};
