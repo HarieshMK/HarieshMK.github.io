6fe8cea9072af12791a55d1a21b5565d75daa8cf
HarieshMK <154051561+HarieshMK@users.noreply.github.com>
2026-04-27 11:05:03 +0530

    Update finance-engine.js

--- assets/js/finance-engine.js
+++ assets/js/finance-engine.js
@@ -38,11 +38,11 @@ var FinanceEngine = {
     },
 
     formatIndian: function(num) {
-        if (num >= 10000000) return (num / 10000000).toFixed(2) + " Cr";
-        if (num >= 100000) return (num / 100000).toFixed(2) + " L";
+        if (num >= 10000000) {return (num / 10000000).toFixed(2) + " Cr";}
+        if (num >= 100000) {return (num / 100000).toFixed(2) + " L";}
         return Math.round(num).toLocaleString('en-IN');
     }
-};
+    };
 
 /* Tax Calculation Module */
 FinanceEngine.TaxEngine = {
