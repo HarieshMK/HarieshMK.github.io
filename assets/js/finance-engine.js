3006d278c9af73dd7451cd148e5e0305d1543f7c
HarieshMK <154051561+HarieshMK@users.noreply.github.com>
2026-05-06 10:47:45 +0530

    Update finance-engine.js

--- assets/js/finance-engine.js
+++ assets/js/finance-engine.js
@@ -215,4 +215,5 @@ FinanceEngine.TaxEngine = {
             appliedDeductions: { section80C: cappedOther80C, section80D: totalCapped80D, homeInterest: interest24b + interest80EEA }
         };
     } 
+}
 };
