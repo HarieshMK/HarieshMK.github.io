1bf071a08bf0fcbce6b6cf86ee3142f905415bac
HarieshMK <154051561+HarieshMK@users.noreply.github.com>
2026-09-25 15:30:03 +0530

    Update clhl-calculator.js

--- assets/js/clhl-calculator.js
+++ assets/js/clhl-calculator.js
@@ -831,6 +831,7 @@ rows.forEach((row, index) => {
         } else if (typeSelect === 'Rate Change') {
             closingBalance = previousClosingBalance;
         }
+        }
         if (typeSelect === 'EMI payment') {
             totalPrincipalPaidSum += principalPaid;
             totalInterestPaidSum += interestPaid;
